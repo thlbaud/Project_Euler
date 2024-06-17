bbb5a43a1c5ed568f2bb3531d6b731160855e827
thibaud <thibaud.tme@gmail.com>
2024-06-17 17:28:40 +0200

    13

---
+++ 13_large_sum.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   13_large_sum.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thibaud <thibaud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 16:59:56 by thibaud           #+#    #+#             */
+/*   Updated: 2024/06/17 17:28:04 by thibaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>	
+
+char	*_large_sum(char *num)
+{
+	char	*res;
+	int		i;
+
+	
+	return (res);
+}
+	
+int	main(void)
+{
+	printf("%ld\n", _large_sum());
+	return (1);
+}
+
